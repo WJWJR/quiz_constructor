7fa4fafce296fb0c4fc426ce46b65b60d0cc53ca
William J Weekes Jr <WilliamWeekes@Williams-MacBook-Pro.local>
2017-04-04 06:21:49 -0400

    completed LevelOne

---
+++ index.js
@@ -0,0 +1,22 @@
+//It's okay ask your classmate a Question!
+
+
+
+function Quiz (quizQuestion, correctAnswer, mightBeAnswers) {
+  this.question = quizQuestion;
+  this.correct = correctAnswer;
+  this.mightBe = mightBeAnswers;
+}
+
+let question1 = new Quiz('Does Mary Jane know Javascript?', 'No', ['dances', 'smokes', 'dogs'])
+//console.log(question1);
+
+
+ let questionArray = [question1];
+ console.log(questionArray);
+
+ let source = document.querySelector('#question-template').innerHTML;
+ let template = Handlebars.compile(source);
+ let html = questionArray.map(object => template(object));
+ let destination = document.querySelector('.handlebars-demo');
+  destination.innerHTML = html;
